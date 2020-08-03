251cb374390e670905e7e3592c18ac500c589983
Stephanie M Segura <stephaniemsegura09@gmail.com>
2020-08-03 22:38:00 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,7 +4,6 @@ def find_min_in_nested_arrays(src)
 row_index = 0
 while row_index < src.count do
   element_index = 0
-  binding.pry
   smallest_numbers = src[0]
   while element_index < src[row_index].count do
     if src[row_index][element_index].length > smallest_numbers.length
